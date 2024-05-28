7450e359c5dd9ee664009c0078811ed34026afd6
Ruxadev <grmdev01@gmail.com>
2024-05-28 19:45:25 -0300

    Stocks listing view and Cleanup application Layout

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,11 +6,16 @@ class Stock < ApplicationRecord
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
-                                endpoint: 'https://cloud.iexapis.com/v1')
+                                  endpoint: 'https://cloud.iexapis.com/v1')
     begin
-      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+      company_name = client.company(ticker_symbol).company_name
+      last_price = client.price(ticker_symbol)
+      puts "Company Name: #{company_name}, Last Price: #{last_price}" # Debug statement
+      new(ticker: ticker_symbol, name: company_name, last_price: last_price)
     rescue => exception
+      puts "Error fetching stock information: #{exception.message}" # Debug statement
       return nil
     end
   end
+
 end
